a2e3a09518d70168375c44dd7768ed4307e3f62c
Chainarong Boonsangiem <0864713049@outlook.com>
2024-02-11 00:33:10 +0700

    Add Spring Security (InMemoryUserDetail) Demo

---
+++ Security - Memory Database/src/main/java/com/il0v3dorayaki/demo/controller/HomeController.java
@@ -0,0 +1,15 @@
+package com.il0v3dorayaki.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class HomeController {
+
+    @GetMapping()
+    public String home() {
+        return "Spring Security Demo";
+    }
+}
---
+++ Security - Memory Database/src/main/java/com/il0v3dorayaki/demo/entity/Wallet.java
@@ -0,0 +1,30 @@
+package com.il0v3dorayaki.demo.entity;
+
+public class Wallet {
+
+    private String name;
+
+    private int balance;
+
+    public Wallet(String name, int balance) {
+        this.name = name;
+        this.balance = balance;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getBalance() {
+        return balance;
+    }
+
+    public void setBalance(int balance) {
+        this.balance = balance;
+    }
+
+}
