4bdd77ffa67cc9f37eb614226e0824e75e6de20e
Shinonome Nano <0864713049@outlook.com>
2024-02-14 14:53:00 +0700

    Add Security - JWT Demo

---
+++ Security - JWT Demo/src/main/java/com/il0v3dorayaki/jwtdemo/SecurityWithJwtProjectApplication.java
@@ -0,0 +1,13 @@
+package com.il0v3dorayaki.jwtdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityWithJwtProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurityWithJwtProjectApplication.class, args);
+	}
+
+}
---
+++ Security - JWT Demo/src/test/java/com/il0v3dorayaki/jwtdemo/SecurityWithJwtProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.il0v3dorayaki.jwtdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityWithJwtProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
